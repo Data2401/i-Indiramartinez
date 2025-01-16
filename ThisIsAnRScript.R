cd1378acda58e671d0fbad99351fa29ec8b4af41
Indiramartinez <indirasilva.022@gmail.com>
2025-01-16 12:48:21 -0600

    Update ThisIsAnRScript.R

--- ThisIsAnRScript.R
+++ ThisIsAnRScript.R
@@ -2,8 +2,8 @@
 ## Run this code to make a plot of x and x^2 for the integers from 1 to 10
 
 x <- 1:20
-y <- x^3
+y <- x^(1/2)
 
 
-plot(x,y, type = "b", main = "Cubed integers")
+plot(x,y, type = "b", main = "Squared root of integers")
 
